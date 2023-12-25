66eaa00a96f3e516cb5c168c454de75ced03c6a9
Nurullo <khuwaidullaev@gmail.com>
2023-12-25 17:27:03 +0500

    chore: add cards

---
+++ src/components/Cards/data.js
@@ -0,0 +1,33 @@
+const locationsInfo = [
+  {
+    city: "Damietta",
+    telephone_number: "+20 0219033",
+    email: "alexbank@damietta.com",
+    address:
+      "3 Al Togari, Qism Damietta, Damietta, Damietta Governorate 34511, Egypt",
+  },
+  {
+    city: "Lille",
+    telephone_number: "+33 3 20 54 44 50",
+    email: "theatre-sebastobol@lille.fr",
+    address: "Pl. Sébastopol, 59000 Lille, France",
+  },
+  {
+    city: "Dayton",
+    telephone_number: "+1 937 226 1710",
+    email: "info@americaspackardmuseum.org",
+    address: "420 S Ludlow St, Dayton, OH 45402, United States",
+  },
+  {
+    city: "Kulusuk",
+    telephone_number: "+299 98 69 88",
+    email: "info@mit.gl",
+    address: "HVF8+R82, Kulusuk, Greenland",
+  },
+  {
+    city: "Kanazawa",
+    telephone_number: "+81 76 220 2469",
+    email: "kanazawa@lg.jp",
+    address: "1 Chome Higashiyama, Kanazawa, Ishikawa 920-0831, Japan",
+  },
+];
