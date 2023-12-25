e1dd6d62901ad47002dcf1993657d1093677da94
Nurullo <khuwaidullaev@gmail.com>
2023-12-25 19:44:00 +0500

    chore: add countries' icon

--- src/components/Cards/data.js
+++ src/components/Cards/data.js
@@ -1,3 +1,9 @@
+import egypt from "../../images/egypt_icon.svg";
+import france from "../../images/france_icon.svg";
+import japan from "../../images/japan_icon.svg";
+import malta from "../../images/malta_icon.svg";
+import usa from "../../images/usa_icon.svg";
+
 const locationsInfo = [
   {
     city: "Damietta",
@@ -5,29 +11,36 @@ const locationsInfo = [
     email: "alexbank@damietta.com",
     address:
       "3 Al Togari, Qism Damietta, Damietta, Damietta Governorate 34511, Egypt",
+    icon_path: egypt,
   },
   {
     city: "Lille",
     telephone_number: "+33 3 20 54 44 50",
     email: "theatre-sebastobol@lille.fr",
     address: "Pl. Sébastopol, 59000 Lille, France",
+    icon_path: france,
   },
   {
     city: "Dayton",
     telephone_number: "+1 937 226 1710",
     email: "info@americaspackardmuseum.org",
     address: "420 S Ludlow St, Dayton, OH 45402, United States",
+    icon_path: usa,
   },
   {
     city: "Kulusuk",
     telephone_number: "+299 98 69 88",
     email: "info@mit.gl",
     address: "HVF8+R82, Kulusuk, Greenland",
+    icon_path: malta,
   },
   {
     city: "Kanazawa",
     telephone_number: "+81 76 220 2469",
     email: "kanazawa@lg.jp",
     address: "1 Chome Higashiyama, Kanazawa, Ishikawa 920-0831, Japan",
+    icon_path: japan,
   },
 ];
+
+export { locationsInfo };
